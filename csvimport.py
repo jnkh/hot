24eedd0d0320d90b37c00efb2efaa39894ecb11c
dennizk@gmail.com <denizkahramaner@rescomp-13-175072.stanford.edu>
2014-08-21 02:00:27 -0700

    CSV File scraper 1.0

---
+++ csvimport.py
@@ -0,0 +1,10 @@
+import csv
+with open('../youporn.csv', 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print row
+        print "\n"
+        print "\n"
+        print "\n"
+        print "\n"
+        print "\n"
