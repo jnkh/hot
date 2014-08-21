b0257522535da113613d103434ef1b173a661651
Julian Kates-Harbeck <juliankh@stanford.edu>
2014-08-21 02:54:56 -0700

    parser filters videos for pornstars

--- csvimport.py
+++ csvimport.py
@@ -1,10 +1,14 @@
 import csv
-with open('../youporn.csv', 'rb') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print row
-        print "\n"
-        print "\n"
-        print "\n"
-        print "\n"
-        print "\n"
+import pandas
+import numpy as np
+
+#Data Frame with the fields as in the first line of the file:
+#EMBEDIFRAMECODE  THUMB  TITLE  TAG  CATEGORY  PORNSTAR  DURATION
+frame = pandas.io.parsers.read_csv('youporn.csv',sep='|')
+
+#Mask for all entries that have a pornstar named
+with_pornstars_mask = pandas.notnull(frame.PORNSTAR)
+
+#embed codes and pornstars of those entries
+embed_codes = np.array(frame.EMBEDIFRAMECODE[with_pornstars_mask])
+pornstars = np.array(frame.PORNSTAR[with_pornstars_mask])
